b00a4ef6f975f73cd17a4734a3f7528d10d5c167
galalkoro98 <galal.koro@gmail.com>
2023-09-30 21:51:59 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import { Main } from "./Main/Main";
 import { Header } from "./Home/Header";
 import { Controls } from "./Controllers/Controllers";
+import Dom from "./Hooks/Dom/Dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Controls />
     </div>
   );
